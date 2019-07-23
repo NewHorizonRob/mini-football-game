bee3a625c2fb7e6df6901960190f0744181944f0
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-23 15:55:47 -0400

    Add files via upload

---
+++ FootballTeam.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Football
+{
+    class FootballTeam
+    {
+        int score;
+        string name;
+
+        public FootballTeam(string teamname)
+        {
+            score = 0;
+            name = teamname;
+        }
+
+        public void ShowScore()
+        {
+            Console.WriteLine($"{name} has a score of {score}");
+        } 
+
+        public void ScoreTouchDown()
+        {
+            score = score + 6;
+        }
+
+        public void ExtraPoint()
+        {
+            score = score + 1;
+        }
+
+        public void FieldGoal()
+        {
+            score = score + 3;
+        }
+
+        public void Safety()
+        {
+            score = score + 2;
+        } 
+
+        public void TwoPointConversion()
+        {
+            score = score + 2;
+        }
+   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FootballTeam Clemson = new FootballTeam("Clemson");
+            FootballTeam Alabama = new FootballTeam("Alabama");
+
+            Alabama.ScoreTouchDown();
+            Alabama.ExtraPoint();
+            Clemson.ScoreTouchDown();
+            Clemson.ExtraPoint();
+            Clemson.ScoreTouchDown();
+            Clemson.ExtraPoint();
+            Alabama.ScoreTouchDown();
+            Alabama.ExtraPoint();
+            Alabama.ScoreTouchDown();
+            Alabama.ExtraPoint();
+            Clemson.FieldGoal();
+            Clemson.ScoreTouchDown();
+            Clemson.ExtraPoint();
+           // Clemson.ShowScore();
+           // Alabama.ShowScore();
+            Alabama.FieldGoal();
+            Alabama.ScoreTouchDown();
+            Alabama.ExtraPoint();
+            Clemson.FieldGoal();
+           // Clemson.ShowScore();
+           // Alabama.ShowScore();
+            Alabama.ScoreTouchDown();
+            Alabama.ExtraPoint();
+            Clemson.ScoreTouchDown();
+            Alabama.ScoreTouchDown();
+            Alabama.ExtraPoint();
+            Clemson.ScoreTouchDown();
+            Clemson.ExtraPoint();
+            Alabama.ShowScore();
+            Clemson.ShowScore();
+
+
+
+
+
+            Console.WriteLine("Pak");
+            Console.ReadKey();
+            
+        }
+    }
+}
