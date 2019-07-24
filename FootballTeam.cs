af250c5ffd3b5e7bea4e0d60c6b04b7d5687484c
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 01:17:13 -0400

    Update FootballTeam.cs

--- FootballTeam.cs
+++ FootballTeam.cs
@@ -8,43 +8,43 @@ namespace Ch4_Football
 {
     class FootballTeam
     {
-        int score;
-        string name;
+        int score; #creates the int variable score 
+        string name; # creates the string variable name 
 
-        public FootballTeam(string teamname)
+        public FootballTeam(string teamname) #method FootballTeam with a string as a parameter
         {
-            score = 0;
-            name = teamname;
+            score = 0;   #assigns the score variable to zero
+            name = teamname; # assigns the name to teamname 
         }
 
-        public void ShowScore()
+        public void ShowScore() #method used for showing the score 
         {
-            Console.WriteLine($"{name} has a score of {score}");
+            Console.WriteLine($"{name} has a score of {score}"); #writes the score of the game to the console w/ formatted string
         } 
 
-        public void ScoreTouchDown()
+        public void ScoreTouchDown() #method for adding six to the team scoring a touchdown
         {
-            score = score + 6;
+            score = score + 6; #adds six to the total score
         }
 
-        public void ExtraPoint()
+        public void ExtraPoint() #method for adding one to the team scoring a field goal
         {
-            score = score + 1;
+            score = score + 1; #adds one to the total score
         }
 
-        public void FieldGoal()
+        public void FieldGoal() #method for adding three to the team scoring a field goal
         {
-            score = score + 3;
+            score = score + 3; #adds three to the total score
         }
 
-        public void Safety()
+        public void Safety() #method for adding two to the team scoring a safety
         {
-            score = score + 2;
+            score = score + 2; #adds two to the total score
         } 
 
-        public void TwoPointConversion()
+        public void TwoPointConversion() #method for adding two to the team scoring a two point conversion 
         {
-            score = score + 2;
+            score = score + 2; #adds two to the total score
         }
    
     }
